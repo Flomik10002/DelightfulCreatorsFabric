0c2c47035a0aef670568aab0185102a826a902f4
Flomik <79540285+Flomik10002@users.noreply.github.com>
2023-06-13 16:14:16 +0400

    0.0.1v

--- src/main/java/flomik/delightfulcreators/init/utils/tags/ModTags.java
+++ src/main/java/flomik/delightfulcreators/init/utils/tags/ModTags.java
@@ -1,6 +1,7 @@
 package flomik.delightfulcreators.init.utils.tags;
 
 
+import flomik.delightfulcreators.DelightfulCreatorsMod;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -18,6 +19,10 @@ public class ModTags {
     public static class ModFluidTags {
 
         public static final TagKey<Fluid> TOMATO_SAUCE = createCommonFluidTag("tomato_sauce");
+
+        public static @NotNull TagKey<Fluid> createFluidTag(String modFluidTagName) {
+            return TagKey.of(Registry.FLUID_KEY, new Identifier(DelightfulCreatorsMod.MOD_ID, modFluidTagName));
+        }
 
         public static @NotNull TagKey<Fluid> createCommonFluidTag(String modFluidTagName) {
             return TagKey.of(Registry.FLUID_KEY, new Identifier("c", modFluidTagName));
