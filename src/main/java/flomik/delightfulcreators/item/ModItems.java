0e3bae27cc7028440ddf8ea4b77cd626ad66e055
Flomik <79540285+Flomik10002@users.noreply.github.com>
2023-06-18 17:10:44 +0400

    Fix hunger and saturation

--- src/main/java/flomik/delightfulcreators/item/ModItems.java
+++ src/main/java/flomik/delightfulcreators/item/ModItems.java
@@ -11,25 +11,25 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     public static final Item INCOMPLETE_PASTA_WITH_MEATBALLS = registerItem("incomplete_pasta_with_meatballs",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_PASTA_WITH_MUTTON_CHOP = registerItem("incomplete_pasta_with_mutton_chop",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_EGG_SANDWICH = registerItem("incomplete_egg_sandwich",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_CHICKEN_SANDWICH = registerItem("incomplete_chicken_sandwich",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_HAMBURGER = registerItem("incomplete_hamburger",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_BACON_SANDWICH = registerItem("incomplete_bacon_sandwich",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_MUTTON_WRAP = registerItem("incomplete_mutton_wrap",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_PUMPKIN_PIE = registerItem("incomplete_pumpkin_pie",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_SWEET_BERRY_CHEESECAKE = registerItem("incomplete_sweet_berry_cheesecake",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item INCOMPLETE_APPLE_PIE = registerItem("incomplete_apple_pie",
-            new Item(new FabricItemSettings().group(ModItemsGroup.main)));
+            new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
     public static final Item PUMPKIN_PIE_SLICE = registerItem("pumpkin_pie_slice",
             new Item(new FabricItemSettings().group(ModItemsGroup.main).food(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build())));
 
